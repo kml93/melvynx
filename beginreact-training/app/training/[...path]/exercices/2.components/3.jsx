7a221598cbe197de0d462403f864c02c4cb34c65
Jacky Daniel's <kml93@msn.com>
2024-07-16 11:08:44 +0200

    beginreact-training => Finish Exercice 3.6

--- beginreact-training/app/training/[...path]/exercices/2.components/3.jsx
+++ beginreact-training/app/training/[...path]/exercices/2.components/3.jsx
@@ -1,24 +1,28 @@
-import clsx from 'clsx';
+import { cva } from 'class-variance-authority';
 
-const Badge = ({ size = 'default', variant = 'red', children }) => {
-  return (
-    <span
-      className={clsx(
-        'inline-flex w-fit items-center rounded-md text-sm font-medium',
-        {
-          'px-2 py-1': size === 'lg',
-          'px-1.5 py-0.5': size === 'default',
-        },
-        {
-          'bg-red-500/10 text-red-700': variant === 'red',
-          'bg-green-500/10 text-green-700': variant === 'green',
-          'bg-purple-500/10 text-purple-700': variant === 'purple',
-        }
-      )}
-    >
-      {children}
-    </span>
-  );
+const badgeVariants = cva(
+  'inline-flex w-fit items-center rounded-md text-sm font-medium',
+  {
+    variants: {
+      size: {
+        default: 'px-1.5 py-0.5',
+        lg: 'px-2 py-1',
+      },
+      color: {
+        red: 'bg-red-500/10 text-red-700',
+        green: 'bg-green-500/10 text-green-700',
+        purple: 'bg-purple-500/10 text-purple-700',
+      },
+    },
+    defaultVariants: {
+      size: 'default',
+      color: 'red',
+    },
+  }
+);
+
+const Badge = ({ size, color, children }) => {
+  return <span className={badgeVariants({ size, color })}>{children}</span>;
 };
 
 export default function App() {
@@ -29,23 +33,23 @@ export default function App() {
       <p className="code">Red</p>
       <p className="code">Purple</p>
       <p className="code">lg</p>
-      <Badge size="lg" variant="green">
+      <Badge size="lg" color="green">
         New
       </Badge>
-      <Badge size="lg" variant="red">
+      <Badge size="lg" color="red">
         New
       </Badge>
-      <Badge size="lg" variant="purple">
+      <Badge size="lg" color="purple">
         New
       </Badge>
       <p className="code">default</p>
-      <Badge size="default" variant="green">
+      <Badge size="default" color="green">
         New
       </Badge>
-      <Badge size="default" variant="red">
+      <Badge size="default" color="red">
         New
       </Badge>
-      <Badge size="default" variant="purple">
+      <Badge size="default" color="purple">
         New
       </Badge>
     </div>
