fa2a609a1615616aa8269af2cac0f86daf27d077
Jacky Daniel's <kml93@msn.com>
2024-07-24 21:35:00 +0200

    beginreact-training => Finish Exercice 8.1

--- beginreact-training/app/training/[...path]/exercices/4.use-effect/8.jsx
+++ beginreact-training/app/training/[...path]/exercices/4.use-effect/8.jsx
@@ -1,20 +1,21 @@
-// @ts-nocheck
-"use client";
+'use client';
 
-import { User2 } from "lucide-react";
-import { useState } from "react";
+import { User2 } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
-// 🦁 Créer une clé `STORAGE_KEY` qui est égale à `storage-name`
+const STORAGE_KEY = 'storage-name';
 
 const NameForm = ({ initialName }) => {
-  // 🦁 Comme valeur initiale, récupère la valeur dans le localStorage
-  const [name, setName] = useState(initialName);
+  const [name, setName] = useState(
+    () => localStorage.getItem(STORAGE_KEY) ?? initialName
+  );
 
-  // 🦁 Créer un `useEffect` avec `name` comme dépendance
-  // 🦁 Sauvegarde le `name` dans le localStorage avec la clé définie dans `STORAGE_KEY`
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEY, name);
+  }, [name]);
 
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center space-y-4">
       <label className="input input-bordered flex items-center gap-2">
         <User2 size={16} />
         <input
@@ -32,7 +33,7 @@ const NameForm = ({ initialName }) => {
 export default function App() {
   return (
     <div className="flex flex-col gap-8">
-      <NameForm initialName={"Jean"} />
+      <NameForm initialName={'Jean'} />
     </div>
   );
 }
