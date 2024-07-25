ef769da620806a15fb00ef58d299c70656235c82
Jacky Daniel's <kml93@msn.com>
2024-07-25 14:51:35 +0200

    beginreact-training => Finish Exercice 9.3

--- beginreact-training/app/training/[...path]/exercices/4.use-effect/9.jsx
+++ beginreact-training/app/training/[...path]/exercices/4.use-effect/9.jsx
@@ -2,18 +2,39 @@
 
 import { useEffect, useState } from 'react';
 
+const useEventListener = ({
+  eventName,
+  handler,
+  element = window,
+  enabled = true,
+}) => {
+  useEffect(() => {
+    if (!enabled) return;
+
+    const onClick = () => handler();
+
+    console.log('Add EventListener');
+    element.addEventListener(eventName, onClick);
+
+    return () => {
+      console.log('Remove EventListener');
+      element.removeEventListener(eventName, onClick);
+    };
+  }, [element, enabled, eventName, handler]);
+};
+
 export default function App() {
   const [isCountingClick, setIsCountingClick] = useState(false);
   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    if (!isCountingClick) return;
-
-    const handleClick = () => setCount((curr) => curr + 1);
-    window.addEventListener('click', handleClick);
-
-    return () => window.removeEventListener('click', handleClick);
-  }, [isCountingClick]);
+  useEventListener({
+    eventName: 'click',
+    handler: () => {
+      console.log('Click window');
+      setCount((curr) => curr + 1);
+    },
+    enabled: isCountingClick,
+  });
 
   return (
     <div className="mx-auto flex max-w-sm flex-col items-center gap-8">
