fbb36ef5eeeaf9056441b5df2cf656a3d1acb73c
Jacky Daniel's <kml93@msn.com>
2024-07-26 09:58:17 +0200

    3-useEffect => Init project & Finish partie 1/4

---
+++ 3-useEffect/src/app/layout.tsx
@@ -0,0 +1,23 @@
+import clsx from "clsx";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Movie Finder",
+  description: "Find movie in OMDb database",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full" data-theme="dracula">
+      <body className={clsx(inter.className, "h-full")}>{children}</body>
+    </html>
+  );
+}
---
+++ 3-useEffect/src/app/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const Page = () => {
+  return <main className="min-h-full">Home</main>;
+};
+
+export default Page;
